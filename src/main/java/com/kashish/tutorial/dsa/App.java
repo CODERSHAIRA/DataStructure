cb05712ee720e361a0236fbb0d81128941e0ddd4
Coder Shaira <kangiakashish2001@gmail.com>
2022-12-04 22:38:34 +0530

    added code for dsa.

---
+++ src/main/java/com/kashish/tutorial/dsa/App.java
@@ -0,0 +1,21 @@
+package adt;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Counter myCounter=new Counter("Shaira");
+myCounter.increment();
+myCounter.increment();
+myCounter.increment();
+myCounter.increment();
+myCounter.increment();
+myCounter.increment();
+myCounter.increment();
+
+System.out.println(myCounter.getCurrentValue());
+System.out.println(myCounter.toString());
+
+	}
+
+}
---
+++ src/main/java/com/kashish/tutorial/dsa/Counter.java
@@ -0,0 +1,26 @@
+package adt;
+
+public class Counter {
+    static String name=null;
+	static int i=0;
+    
+	
+	public Counter(String str) {
+		// TODO Auto-generated constructor stub
+		this.name=str;
+	}
+	
+	public static void increment() {
+		i++;
+	}
+
+	public static int getCurrentValue() {
+		return i;
+	}
+
+
+	public String toString() {
+		return name;
+	}
+
+}

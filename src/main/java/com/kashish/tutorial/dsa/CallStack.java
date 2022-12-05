1a0e66b9e2881072ad391837b0950a102f8a5ebb
Coder Shaira <kangiakashish2001@gmail.com>
2022-12-05 08:16:22 +0530

    added a stack code.

---
+++ src/main/java/com/kashish/tutorial/dsa/CallStack.java
@@ -0,0 +1,24 @@
+package com.kashish.tutorial.dsa;
+
+public class CallStack {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+				
+				Stack theStack=new Stack(5);
+				theStack.push(1);
+				theStack.push(2);
+				theStack.push(3);
+				theStack.push(4);
+				theStack.push(5);
+				
+				while(!theStack.isEmpty()){
+					long value=theStack.pop();
+				System.out.println(value);
+				}
+				
+			}
+	}
+
+
---
+++ src/main/java/com/kashish/tutorial/dsa/Stack.java
@@ -0,0 +1,58 @@
+package com.kashish.tutorial.dsa;
+
+public class Stack {
+
+	
+		// TODO Auto-generated constructor stub
+	
+			
+			private int maxSize;
+			private long[] stackArray;
+			private int top;
+			
+			public Stack(int size){
+				
+				this.maxSize=size;
+				this.stackArray= new long[maxSize];
+				this.top=-1;
+				
+			}
+			public void push(long j){
+				if(isFull()){
+					System.out.println("The stack is already Full");
+				}
+				else{
+				top++;
+				stackArray[top] =j;
+				}
+				
+			}
+			public long pop(){
+				if(isEmpty()){
+					System.out.println("The stack is already empty");
+					return -1;
+				}
+				else{
+				int old_top=top;
+				top--;
+				return stackArray[old_top];
+				}
+			}
+			public long peek(){
+				return stackArray[top];
+				
+			}
+			public boolean isEmpty(){
+				return(top==-1);
+			}
+				public boolean isFull(){
+				return(top==maxSize-1);
+			}
+			
+				
+			
+				
+		}
+	
+
+
